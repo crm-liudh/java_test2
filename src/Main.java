db90bb8dc816f18bbc0b0f90eda501f690586dff
xiaoxuan <1543002286@qq.com>
2022-03-15 18:35:56 +0800

    homework

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("3333");
-        System.out.println("4444");
-        System.out.println("2222");
-    }
-}
